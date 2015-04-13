0beb21ead24c2666f4df171f0171994098af5df5
Eric Saxby <sax@livinginthepast.org>
2015-04-13 14:15:45 -0700

    Raise if no reservation is configured in data bag

--- libraries/port_reservation.rb
+++ libraries/port_reservation.rb
@@ -1,9 +1,13 @@
 module PortReservation
   extend Chef::DSL::DataQuery
 
+  class MissingReservation < StandardError; end
+
   class << self
     def for(type)
-      reservations[type]
+      reservations[type].tap do |reservation|
+        raise MissingReservation.new("No port reservation found for #{type}") if reservation.nil?
+      end
     end
 
     def reservations
