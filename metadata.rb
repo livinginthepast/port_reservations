46388defc2b49e6d431f59220532f441e0cac8c7
Eric Saxby <sax@livinginthepast.org>
2015-04-13 14:15:54 -0700

    0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'sax@livinginthepast.org'
 license          'MIT'
 description      'Helpers for reserving ports to different applications'
 long_description 'Helpers for reserving ports to different applications'
-version          '0.1.0'
+version          '0.2.0'
 
