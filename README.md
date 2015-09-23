d77e6216cf5999a62bcc06cd49cdaee3d72c954f
Eric Saxby & James Hart <eric+james@wanelo.com>
2015-09-23 11:13:19 -0700

    Port reservations can be ranges, check for ambiguous reservations

--- README.md
+++ README.md
@@ -24,6 +24,22 @@ touch data_bags/ports/reservations.json
 }
 ```
 
+In order to reserve a range of ports, strings can be used:
+
+```json
+{
+  "id": "reservations",
+  "mapping": {
+    "my-app": 8000,
+    "haproxy-for-my-app": 8001
+    "redii": "30000..30012"
+  }
+}
+```
+
+Note that when in this case, a range is returned by the included helpers.
+
+
 ### Helpers
 
 In a recipe, you can use the `PortReservation` helper to retrieve a specific port.
@@ -32,5 +48,6 @@ In a recipe, you can use the `PortReservation` helper to retrieve a specific por
 include_recipe 'port_reservations'
 
 PortReservation.for('my-app') == 8000
+PortReservation.for('redii') == 30000..30012
 ```
 
--- libraries/port_reservation.rb
+++ libraries/port_reservation.rb
@@ -2,16 +2,45 @@ module PortReservation
   extend Chef::DSL::DataQuery
 
   class MissingReservation < StandardError; end
+  class AmbiguousReservation < StandardError; end
 
   class << self
     def for(type)
-      reservations[type].tap do |reservation|
+      ip = reservations[type].tap do |reservation|
         raise MissingReservation.new("No port reservation found for #{type}") if reservation.nil?
+        raise AmbiguousReservation.new("port #{reservation} is reserved by multiple services") if ambiguous?(reservation)
       end
+      ip.is_a?(String) ? string_to_range(ip) : ip
     end
 
     def reservations
       @reservations ||= data_bag_item('ports', 'reservations')['mapping']
+    end
+
+    def ambiguous?(reservation)
+      reservations_count_by_ip[reservation] > 1
+    end
+
+    def reservations_count_by_ip
+      count_by_ip ||= begin
+        count = Hash.new(0)
+        reservations.values.each do |res|
+          case res
+          when String
+            string_to_range(res).each do |ip|
+              count[ip] += 1
+            end
+          else
+            count[res] += 1
+          end
+        end
+        count
+      end
+    end
+
+    def string_to_range(string)
+      boundaries = string.split('..').map { |d| Integer(d) }
+      boundaries[0]..boundaries[1]
     end
   end
 end
